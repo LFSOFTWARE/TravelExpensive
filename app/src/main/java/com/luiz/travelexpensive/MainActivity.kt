24f9fc0c218ad1be655a4122b08a3db8a323a4cd
Luiz <luiz.ssantos.ext@infracommerce.com.br>
2023-09-08 22:03:22 -0300

    feat: event click

--- app/src/main/java/com/luiz/travelexpensive/MainActivity.kt
+++ app/src/main/java/com/luiz/travelexpensive/MainActivity.kt
@@ -2,17 +2,29 @@ package com.luiz.travelexpensive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.luiz.travelexpensive.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
-    private fun calculate(){
 
+        binding.buttonCalcular.setOnClickListener(this)
+    }
+
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calcular) {
+            calculate()
+        }
+    }
+
+    private fun calculate() {
+        Toast.makeText(this,"Calculado",Toast.LENGTH_SHORT).show()
     }
 }
