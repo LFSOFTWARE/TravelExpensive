33dd188d7b68c148f6ae7e5eb29b548c1db612d8
Luiz <luiz.ssantos.ext@infracommerce.com.br>
2023-09-08 21:40:03 -0300

    feat: add view binding

--- app/src/main/java/com/luiz/travelexpensive/MainActivity.kt
+++ app/src/main/java/com/luiz/travelexpensive/MainActivity.kt
@@ -2,10 +2,17 @@ package com.luiz.travelexpensive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.luiz.travelexpensive.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+    private fun calculate(){
+
     }
 }
